944e07914479224f6ddfff743f16cfbd7fa2c7f3
sahil16973 <sahil.16973@sakec.ac.in>
2024-10-23 14:40:20 +0530

    Update CircleCalculator.java

--- CircleCalculator.java
+++ CircleCalculator.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
-
 public class CircleCalculator {
-    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Input: Radius of the circle
-        System.out.print("Enter the radius of the circle: ");
-        double radius = scanner.nextDouble();
-        
+        if (args.length < 1) {
+            System.out.println("Please provide the radius as a command line argument.");
+            return;
+        }
+
+        double radius = Double.parseDouble(args[0]);
+
         // Calculating area and perimeter
         double area = Math.PI * Math.pow(radius, 2);
         double perimeter = 2 * Math.PI * radius;
-        
+
         // Output: Area and perimeter
         System.out.printf("Area of the circle: %.2f\n", area);
         System.out.printf("Perimeter (Circumference) of the circle: %.2f\n", perimeter);
-        
-        scanner.close();
     }
 }
